331beec21fac85902820fb6376dd200c6ab2bf4e
Fred Brooks <fred@sma2.rain.com>
2018-09-23 17:54:14 -0700

    add DIO files

---
+++ examples/rn4020_riot/test.X/tpic6b595.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   tpic6b595.h
+ * Author: root
+ *
+ * Created on September 18, 2018, 2:56 PM
+ */
+
+#ifndef TPIC6B595_H
+#define	TPIC6B595_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TPIC6B595_H */
+
